fcd2d146fdde812becf0cf59cdf2445b290ce7dc
subhradipsinha <subhradip.sinha@gmail.com>
2019-11-27 19:20:45 +0530

    apk has been added

--- src/main/java/com/demo/DriverManagerfile.java
+++ src/main/java/com/demo/DriverManagerfile.java
@@ -14,9 +14,10 @@ public class DriverManagerfile {
     public static void setup() throws Exception {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("no", true);
+        
         //desiredCapabilities.setCapability("newCommandTimeout", 100000);
         desiredCapabilities.setCapability("noReset", true);
-        desiredCapabilities.setCapability(MobileCapabilityType.APP, "E:\\Appium_project\\com_nEXCITIZEN_demo\\Driver\\nexcitizen.apk");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "E:\\Appium_project\\com_nEXCITIZEN_demo\\Driver\\nexcitizen_27_11_2019.apk");
         System.out.println("Url hit");
         desiredCapabilities.setCapability(MobileCapabilityType.UDID, "emulator-5554");
         //caps.setCapability("platformName","ANDROID");
