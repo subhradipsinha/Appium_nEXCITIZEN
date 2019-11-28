3dfa7f59e77bf06ff370fcb1393535d4beee15fb
subhradipsinha <subhradip.sinha@gmail.com>
2019-11-28 17:10:09 +0530

    13th commit

--- src/test/java/com/PageObject/New_Registration/Login_Page.java
+++ src/test/java/com/PageObject/New_Registration/Login_Page.java
@@ -6,6 +6,8 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import sun.plugin2.message.Message;
 
 public class Login_Page extends DriverManagerfile {
 
@@ -30,8 +32,14 @@ public class Login_Page extends DriverManagerfile {
         //Click Submit Button Click
         MobileElement el3 = (MobileElement) driver.findElementById("com.nex.nexcitizen:id/login");
         el3.click();
-        Assert.assertEquals(el1,"Without Email ID Successfully Login");
-        Thread.sleep(2000);
+        //Declaration of soft assert object
+        SoftAssert sostassert = new SoftAssert();
+        String accual = "without Username";
+        String expected ="Successfully Login";
+        sostassert.assertEquals(accual, expected);
+        sostassert.assertAll();
+
+
 
     }
     @Test(priority = 2)
@@ -52,6 +60,12 @@ public class Login_Page extends DriverManagerfile {
         Assert.assertEquals(el1,"Without Password Successfully Login");
         System.out.println("Submit button click");
 
+        //Declaration of soft assert object
+        SoftAssert sostassert1 = new SoftAssert();
+        String accual1 = "Without Password";
+        String expected1 ="Successfully Login";
+        sostassert1.assertEquals(accual1, expected1);
+        sostassert1.assertAll();
     }
     @Test(priority = 3)
     public void UserTest() throws Exception {
@@ -74,7 +88,16 @@ public class Login_Page extends DriverManagerfile {
         Assert.assertEquals(el1,"With UserName & Password Un-Successfully Login");
         Thread.sleep(3000);
 
+        //Declaration of soft assert object
+        SoftAssert sostassert2 = new SoftAssert();
+        String accual2 = "Put UserName & Password";
+        String expected2 ="Un-Successfully Login";
+        sostassert2.assertEquals(accual2, expected2);
+        sostassert2.assertAll();
+
     }
+
+
 
     public void quite(){
         System.out.println("Testing complete");
