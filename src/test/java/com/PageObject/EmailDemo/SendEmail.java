e6a455ce51881620979d30d044d73809631e0574
subhradipsinha <subhradip.sinha@gmail.com>
2019-12-13 18:08:07 +0530

    61th commit

--- src/test/java/com/PageObject/EmailDemo/SendEmail.java
+++ src/test/java/com/PageObject/EmailDemo/SendEmail.java
@@ -35,7 +35,7 @@ public class SendEmail {
             Multipart multipart = new MimeMultipart();
             multipart.addBodyPart(messageBodyPart);
             messageBodyPart = new MimeBodyPart();
-            String filename = "Output/emailable-report.html";
+            String filename = "target/surefire-reports/emailable-report.html";
             FileDataSource source =  new FileDataSource(filename);
             messageBodyPart.setDataHandler(new DataHandler((source)));
             messageBodyPart.setFileName(filename);
